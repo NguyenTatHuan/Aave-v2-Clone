17a32d0db729f196befc235b1bb80ae4bac19456
NguyenTatHuan <159444791+NguyenTatHuan@users.noreply.github.com>
2025-11-08 12:02:15 +0700

    Adding tokenization

---
+++ smart-contract/contracts/interfaces/ICreditDelegationToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface ICreditDelegationToken {
+    event BorrowAllowanceDelegated(
+        address indexed fromUser,
+        address indexed toUser,
+        address asset,
+        uint256 amount
+    );
+
+    function approveDelegation(address delegatee, uint256 amount) external;
+
+    function borrowAllowance(
+        address fromUser,
+        address toUser
+    ) external view returns (uint256);
+}
